78c53f937fe52ffb96230aec62cfa15446d3055a
Tyler Gale <ttgale@gmail.com>
2017-05-29 15:36:28 +1000

    First commit
    First commit

---
+++ js/bg.js
@@ -0,0 +1,3 @@
+// Randomly load an image from the imgs folder.  To add new images, simply add them to the var below.
+var images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg'];
+$('html').css({'background-image': 'url(imgs/' + images[Math.floor(Math.random() * images.length)] + ')'});
---
+++ js/clock.js
@@ -0,0 +1,16 @@
+// Responsible for the clock on the start page that is rendered via the div=txt
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
